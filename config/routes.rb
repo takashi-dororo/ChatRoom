84b42edc448b619cf55fb391f20af144c9a83ee1
takashi-dororo <dororo5065@gmail.com>
2018-11-16 22:02:06 +0900

    add users controller action and test

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,9 @@
 Rails.application.routes.draw do
   root 'static_pages#home'
+
+  post   '/login',  to: 'users#create'
+  patch  '/update', to: 'users#update'
+  delete '/logout', to: 'users#destroy'
+
+  resources :users
 end
---
+++ spec/controllers/users_controller_spec.rb
@@ -0,0 +1,26 @@
+require 'rails_helper'
+
+RSpec.describe UsersController, type: :controller do
+
+  describe "GET #create" do
+    it "returns http success" do
+      get :create
+      expect(response).to be_successful
+    end
+  end
+
+  describe "GET #update" do
+    it "returns http success" do
+      get :update
+      expect(response).to be_successful
+    end
+  end
+
+  describe "GET #destroy" do
+    it "returns http success" do
+      get :destroy
+      expect(response).to be_successful
+    end
+  end
+
+end
