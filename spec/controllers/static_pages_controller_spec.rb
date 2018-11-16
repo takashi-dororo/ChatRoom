de8daa2870a565518169e25ed80fba054498034d
takashi-dororo <dororo5065@gmail.com>
2018-11-16 21:41:36 +0900

    add static pages controller rspec

--- spec/controllers/static_pages_controller_spec.rb
+++ spec/controllers/static_pages_controller_spec.rb
@@ -5,7 +5,7 @@ RSpec.describe StaticPagesController, type: :controller do
   describe "GET #home" do
     it "returns http success" do
       get :home
-      expect(response).to have_http_status(:success)
+      expect(response).to be_successful
     end
   end
 
