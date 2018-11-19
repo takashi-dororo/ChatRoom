f30a8ff5dfde2a4c7803bf8c17a240037f986187
takashi-dororo <dororo5065@gmail.com>
2018-11-19 20:11:59 +0900

    tag model generate

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_11_19_110753) do
+ActiveRecord::Schema.define(version: 2018_11_19_111012) do
 
   create_table "room_tags", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.integer "room_id"
@@ -20,6 +20,12 @@ ActiveRecord::Schema.define(version: 2018_11_19_110753) do
   end
 
   create_table "rooms", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.string "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "tags", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
