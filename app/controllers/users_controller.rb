991c1b4da5f5259920d38eee78a550bafac69e7c
takashi-dororo <dororo5065@gmail.com>
2018-11-19 21:10:45 +0900

    changes controller rooms and chats

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,20 +1,8 @@
 class UsersController < ApplicationController
 
-  def show
-
-  end
-
-  def new
-
-  end
-
   def create
   end
 
-  def edit
-
-  end
-  
   def update
   end
 
--- config/routes.rb
+++ config/routes.rb
@@ -1,23 +1,19 @@
 Rails.application.routes.draw do
   root 'static_pages#home'
 
-  post   '/login',          to: 'users#create'
-  patch  '/update',         to: 'users#update'
-  delete '/logout',         to: 'users#destroy'
+  post   '/login',       to: 'users#create'
+  patch  '/update',      to: 'users#update'
+  delete '/logout',      to: 'users#destroy'
 
-  get    '/sessions/new',   to: 'sessions#new'
-  post   '/sessions',       to: 'sessions#create'
-  delete '/sessions/:id',   to: 'sessions#destroy'
+  get    '/rooms/index', to: 'rooms#index'
+  get    '/rooms/:id',   to: 'rooms#show'
+  get    '/rooms/new',   to: 'rooms#new'
+  post   '/rooms',       to: 'rooms#create'
+  delete '/rooms/:id',   to: 'rooms#destroy'
 
-  # get    '/posts/:id',      to: 'posts#show'
-  # get    '/posts',          to: 'posts#index'
-  # get    '/posts/new',      to: 'posts#new'
-  # post   '/posts',          to: 'posts#create'
-  # get    '/posts/:id/edit', to: 'posts#edit'
-  # patch  '/posts/:id',      to: 'posts#update'
-  # delete '/posts/:id',      to: 'posts#destroy'
+  post   '/chats',       to: 'chats#create'
 
   resources :users
-  resources :sessions
-  resources :posts
+  resources :rooms
+
 end
